4772c1a6c48c80ec29c46dc759235b33d1436f91
xhed404 <131288012+xhed404@users.noreply.github.com>
2026-05-04 22:58:13 +0300

    Create ModrinthDialog.h

---
+++ src/ui/ModrinthDialog.h
@@ -0,0 +1,45 @@
+#pragma once
+ 
+#include "ui/Theme.h"
+ 
+#include <QtCore/QPointer>
+#include <QtCore/QString>
+#include <QtNetwork/QNetworkAccessManager>
+#include <QtNetwork/QNetworkReply>
+ 
+#include <QDialog>
+#include <QLabel>
+#include <QLineEdit>
+#include <QListWidget>
+#include <QPushButton>
+ 
+namespace blockforge
+{
+class ModrinthDialog final : public QDialog
+{
+public:
+explicit ModrinthDialog(UiLang lang, QWidget* parent = nullptr);
+ 
+void setInstanceContext(QString instanceId, QString mcVersion, QString loaderKey, QString modsDir);
+ 
+private:
+void startSearch(const QString& query);
+void startInstallSelected();
+ 
+UiLang m_lang = UiLang::Ru;
+QString m_instanceId;
+QString m_mcVersion;
+QString m_loaderKey;
+QString m_modsDir;
+ 
+QNetworkAccessManager m_net;
+QPointer<QNetworkReply> m_activeReply;
+ 
+QLineEdit* m_searchEdit = nullptr;
+QPushButton* m_searchBtn = nullptr;
+QListWidget* m_results = nullptr;
+QPushButton* m_installBtn = nullptr;
+QLabel* m_status = nullptr;
+};
+}
+Theme.cpp
